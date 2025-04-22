3d8c10248454f6e30493442a16710b56ff8c704f
tomkel <520225+tomkel@users.noreply.github.com>
2025-04-22 02:57:51 -0700

    successfully updateAsset with timestamp from sidecar

--- fix-gphotos-metadata.ts
+++ fix-gphotos-metadata.ts
@@ -5,13 +5,16 @@ import {
 	AssetOrder,
 	TimeBucketSize,
 	getTimeBucket,
+	updateAsset,
 } from "@immich/sdk";
 import { exit } from "node:process";
+import { DateTime } from "luxon";
 
 const apiKey = "21TDFYiI1CtfeuxxlLNGHHCDVpg97ZwwrjWmAnG48M";
 init({ baseUrl: "http://192.168.1.200:2283/api", apiKey });
 
 const album2014 = "6e966c34-9590-48fd-8592-0fc2c885d2c7";
+const gphotosFolder = "/tmp/imm/Photos from 2014/";
 
 // https://immich.app/docs/api/search-assets
 // https://immich.app/docs/api/get-album-info
@@ -28,7 +31,6 @@ async function main() {
 	});
 	// console.log(buckets);
 	console.log("got", buckets.length, "buckets");
-
 	// await Bun.write("buckets.json", JSON.stringify(buckets, null, 2));
 	// console.log('wrote buckets.json');
 
@@ -45,14 +47,16 @@ async function main() {
 		timeBucket: first.timeBucket,
 		withStacked: false, // if false, "stack" prop is always null. if true, returns fewer items, one per stack.
 	});
-	// console.log(bucket);
-
-	// await Bun.write("bucket.json", JSON.stringify(bucket, null, 2));
-	// console.log("wrote bucket.json");
+	console.log("got", bucket.length, "items in bucket", first.timeBucket);
+	await Bun.write("bucket.json", JSON.stringify(bucket, null, 2));
+	console.log("wrote bucket.json");
 
 	bucket.some(async (asset) => {
 		console.log("checking", asset.originalFileName);
-		await getTimeFromSidecar(asset.originalFileName);
+		const unixTimestamp = await getTimeFromSidecar(asset.originalFileName);
+		console.log("unix timestamp from sidecar:", unixTimestamp);
+		await addTimeToAsset(unixTimestamp, asset.id);
+		exit(20);
 	});
 }
 
@@ -99,37 +103,59 @@ interface SupplementalMetadata {
 		androidPackageName: string;
 	};
 }
-async function findSidecar(basename: string) {
-	const uneditedBasename = basename.endsWith("-edited")
-		? basename.slice(0, "-edited".length * -1)
-		: basename;
+async function findSidecar(filename: string) {
+	const uneditedFilename = filename.replace("-edited.jp", ".jp");
 
-	// IMG-20141201-WA0001.jpg.supplemental-metadata.json // might have .jpeg extension
-	for (const ext of ["jpg", "jpeg"]) {
-		const folder = "/tmp/imm/Photos from 2014/";
-		const sidecar = `${folder}${uneditedBasename}.${ext}.supplemental-metadata.json`;
-		const f = Bun.file(sidecar);
-		const exists = await f.exists();
-		console.log(sidecar, exists ? "exists" : "doesnt exist");
-		if (exists) return f;
-	}
-	throw new Error(`sidecar not found for ${basename}`);
+	// IMG-20141201-WA0001.jpg.supplemental-metadata.json
+	// IMG-20141013-WA0001-edited.jpeg // might have .jpeg extension
+	const sidecar = `${gphotosFolder}${uneditedFilename}.supplemental-metadata.json`;
+	const f = Bun.file(sidecar);
+	const exists = await f.exists();
+	console.log(sidecar, exists ? "exists" : "doesnt exist");
+	if (exists) return f;
+
+	throw new Error(`sidecar not found for ${filename}`);
 }
 async function getTimeFromSidecar(filename: string) {
-	let basename: string;
-	if (filename.endsWith(".jpg"))
-		basename = filename.slice(0, ".jpg".length * -1);
-	else if (filename.endsWith(".jpeg"))
-		basename = filename.slice(0, ".jpeg".length * -1);
-	else throw new Error(`unexpected filename ${filename}`);
-	const f = await findSidecar(basename);
+	if (!filename.endsWith(".jpg") && !filename.endsWith(".jpeg"))
+		throw new Error(`unexpected filename ${filename}`);
+	const f = await findSidecar(filename);
 	const sidecar = (await f.json()) as SupplementalMetadata;
 	// console.dir(sidecar, { depth: 5 });
-	const timestamp = sidecar.photoTakenTime.timestamp;
-	if (!timestamp) throw new Error("no timestamp in sidecar");
-	return timestamp;
+	const unixTimestamp = sidecar.photoTakenTime.timestamp;
+	if (!unixTimestamp) throw new Error("no timestamp in sidecar");
+	return unixTimestamp;
 }
 
-function addTimeToAsset() {}
+async function addTimeToAsset(unixTime: string, assetId: string) {
+	const unixTimeAsNum = Number.parseInt(unixTime, 10);
+	if (Number.isNaN(unixTimeAsNum)) throw new Error(`${unixTime} is NaN`);
+
+	const d = new Date(unixTimeAsNum * 1000);
+
+	const isoString = d.toISOString(); // could throw `RangeError: Invalid time value`
+	if (!isoString.startsWith("2014") || !isoString.endsWith("Z"))
+		throw new Error(`timestamp conversion failed with ${assetId}: ${unixTime}`);
+
+	console.log("ISO timestamp: ", isoString);
+	const dateTimeOriginal = `${isoString.slice(0, -1)}+00:00`;
+	console.log("converted to dto", dateTimeOriginal);
+	console.log("assigning to asset:", assetId);
+
+	const r = await updateAsset({
+		id: assetId,
+		updateAssetDto: { dateTimeOriginal },
+	});
+	// fileCreatedAt and localDateTime and exifInfo.dateTimeOriginal will be changed
+	// "fileCreatedAt": "2014-10-06T16:00:00.000Z",
+	// "localDateTime": "2014-10-06T12:00:00.000Z",
+	//  exifInfo."dateTimeOriginal": "2014-10-06T16:00:00+00:00",
+
+	// exifInfo.dateTimeOriginal changes immediately, the others do not
+
+	console.log(r);
+	console.log(r.exifInfo?.dateTimeOriginal);
+	console.log(r.exifInfo?.dateTimeOriginal === dateTimeOriginal);
+}
 
 await main();
